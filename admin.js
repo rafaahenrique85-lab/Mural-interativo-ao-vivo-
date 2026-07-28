3d8d14fbbbf11061ae1bca9d6cdd5d7c644ec8f0
Henrique <rafaahenrique85@gmail.com>
2026-07-28 13:30:40 -0300

    Adicionar formulário de criação de evento

---
+++ admin.js
@@ -0,0 +1,6 @@
+const btnCriarEvento = document.getElementById("btnCriarEvento");
+const formEvento = document.getElementById("formEvento");
+
+btnCriarEvento.addEventListener("click", () => {
+    formEvento.style.display = "block";
+});
