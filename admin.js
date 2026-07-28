75427b54c25dd5445514ef622968f0ef2d2d5afe
Henrique <rafaahenrique85@gmail.com>
2026-07-28 13:41:47 -0300

    Adicionar formulário de criação de evento

--- admin.js
+++ admin.js
@@ -1,6 +1,28 @@
 const btnCriarEvento = document.getElementById("btnCriarEvento");
 const formEvento = document.getElementById("formEvento");
 
+const btnSalvarEvento = document.getElementById("btnSalvarEvento");
+
+const eventoNome = document.getElementById("eventoNome");
+const eventoModo = document.getElementById("eventoModo");
+const eventoTempo = document.getElementById("eventoTempo");
+
 btnCriarEvento.addEventListener("click", () => {
     formEvento.style.display = "block";
+});
+
+btnSalvarEvento.addEventListener("click", () => {
+
+    const nome = document.getElementById("nomeEvento").value;
+
+    const modo = document.querySelector('input[name="aprovacao"]:checked').value;
+
+    const tempo = document.getElementById("tempoEvento").value;
+
+    eventoNome.textContent = nome || "Evento sem nome";
+    eventoModo.textContent = "Modo: " + modo;
+    eventoTempo.textContent = "Tempo de exibição: " + tempo + " segundos";
+
+    formEvento.style.display = "none";
+
 });
